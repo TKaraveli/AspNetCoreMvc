cb49336b4bd99379b6e8c578295c2179851243ae
TKaraveli <tayfunkaraveli@hotmail.com>
2020-01-23 02:36:02 +0300

    json Result kullanıldı.

--- AspNetCoreMvc/Controllers/HomeController.cs
+++ AspNetCoreMvc/Controllers/HomeController.cs
@@ -43,5 +43,47 @@ namespace AspNetCoreMvc.Controllers
 
             return View(employeeListViewModel);
         }
+
+        public StatusCodeResult Index4()
+        {
+            //return StatusCode(200);
+            return Ok();
+        }
+
+        public StatusCodeResult Index5()
+        {
+            //return StatusCode(400);
+            //return BadRequest();
+            return NotFound();
+        }
+
+        public RedirectResult Index6()
+        {
+            return Redirect("/Home/Index3");
+        }
+
+        public IActionResult Index7()
+        {
+            return RedirectToAction("Index2");
+        }
+
+        public IActionResult Index8()
+        {
+            return RedirectToRoute("default");
+        }
+
+        public JsonResult Index9()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee{Id = 1,FirstName = "Tayfun",LastName = "Karaveli", CityId = 6},
+                new Employee{Id = 2,FirstName = "Tayfun2",LastName = "Karaveli2", CityId = 6},
+                new Employee{Id = 3,FirstName = "Tayfun3",LastName = "Karaveli3", CityId = 34}
+            };
+
+            return Json(employees);
+        }
+
+        
     }
 }
